2add5b17321f803352d80537c7c7eb5c92b178a6
caoenze <caoenze@didichuxing.com>
2021-02-24 15:58:49 +0800

    feat: 优化发布脚本

--- script/publish.js
+++ script/publish.js
@@ -10,27 +10,32 @@ let questionsOne = [{
     {name: 'patch(eg. 0.0.X)', value: 'patch'},
     {name: 'minor(eg. 0.X.0)', value: 'minor'},
     {name: 'major(eg. X.0.0)', value: 'major'},
-    {name: 'prerelease(eg. 0.0.1-alpha.x)', value: 'prerelease --preid=alpha'},
+    {name: 'prerelease(eg. 0.0.1-alpha.x)', value: 'prerelease'},
     {name: '无需变更(按照当前设定版本发布)', value: 'unwanted'},
   ]
 }]
 
 inquirer.prompt(questionsOne).then(answers => {
   var type = answers.type
+  console.log(type)
 
   if (type !== 'unwanted') {
     // 变更版本
-    execa.sync('npm', ['version', type]);
+    if (type !== 'prerelease') {
+      execa.sync('npm', ['version', type]);
+    } else {
+      execa.sync('npm', ['version', type, '--preid=alpha']);
+    }
   }
 
-  // try{
-  //   // 切换npm源
-  //   execa.sync('npm', ['config', 'set', 'registry', 'https://registry.npmjs.org/']);
-  //   var { stderr } = child_process.spawnSync('npm', ['publish']);
-  //   console.log('\x1B[46m%s\x1B[0m', stderr);
-  //   console.log('\x1B[42m%s\x1B[0m', 'npm源发布成功');
-  // }catch(error){
-  //   console.log('\x1B[31m%s\x1B[0m', error.message);
-  // }
+  try{
+    // 切换npm源
+    execa.sync('npm', ['config', 'set', 'registry', 'https://registry.npmjs.org/']);
+    var { stderr } = child_process.spawnSync('npm', ['publish']);
+    console.log('\x1B[46m%s\x1B[0m', stderr);
+    console.log('\x1B[42m%s\x1B[0m', 'npm源发布成功');
+  }catch(error){
+    console.log('\x1B[31m%s\x1B[0m', error.message);
+  }
 })
 
